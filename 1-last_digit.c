fafd6b2650c30e4c4e5e40828293516ee3507231
owen20108 <owen20108@yahoo.com>
2023-03-17 16:59:37 -0700

    last

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -1,41 +1,37 @@
-/*
- * File: 1-last_digit.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Prints the last digit of a randomly generated number
- *        and whether it is greater than 5, less than 6, or 0.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 
-	if ((n % 10) > 5)
+	if (last > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
-	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",
-			n, n % 10);
+		printf("Last digit of %d", n);
+		printf(" is %d and is greater than 5\n", last);
 	}
 
+	else if (last < 5 && last != 0)
+	{
+		printf("Last digit of %d", n);
+		printf(" is %d and is less than 6 and not 0\n", last);
+	}
+
+	else if (last == 0)
+	{
+		printf("Last digit of %d", n);
+		printf(" is %d and is 0\n", last);
+	}
 	return (0);
-
-}
+}.
