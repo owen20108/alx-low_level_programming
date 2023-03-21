d1da78ffe9244d3a608f19f2046a6a6705b61bb9
owen20108 <owen20108@yahoo.com>
2023-03-21 13:58:35 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by new line
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	0-putchar('_');
+	0-putchar('p');
+	0-putchar('u');
+	0-putchar('t');
+	0-putchar('c');
+	0-putchar('h');
+	0-putchar('a');
+	0-putchar('r');
+	0-putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,28 @@
+int _putchar(char c);
+
+int _isalpha(int c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int print_sign(int n);
+
+int _abs(int a);
+
+void print_to_98(int n);
+
+int print_last_digit(int a);
+
+void print_to_98(int n);
+
+int add(int a, int b);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+#endif /* _HOLBERTON_H_ */
+
