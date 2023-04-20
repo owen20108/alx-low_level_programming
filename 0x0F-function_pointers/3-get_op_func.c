ffce1219d8c39d79885f445a7a85dbf37c2fa5b1
owen20108 <owen20108@yahoo.com>
2023-04-19 17:17:15 -0700

    op fun

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+/**
+ * get_op_func - finds and points to correct function
+ * @s: the operator string
+ *
+ * Return: 0 or NULL
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (*s == ops[i].op[0])
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
