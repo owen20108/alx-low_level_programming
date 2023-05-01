c9b2d30668b346ef77081060321b2bfab19fa632
owen20108 <owen20108@yahoo.com>
2023-05-01 08:40:44 -0700

    list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
  * get_nodeint_at_index - returns the node at a certain index in a linked list
  * @head: first node in the linked list
  * @index: index of the node to return
- * Return: function that returns the sum of all the data (n) of a listint_t linked list.
+ * Return: returns the sum of all the data (n) of a listint_t linked list.
  * if the list is empty, return 0
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
