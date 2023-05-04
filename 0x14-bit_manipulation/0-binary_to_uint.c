1283496d6b55f3cd1aea31d74f938d4079f642d6
owen20108 <owen20108@yahoo.com>
2023-05-04 15:39:36 -0700

    bin to un

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary number 
+ * to unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int numb;
+	int length, Base_Two;
+
+	if (!b)
+		return (0);
+
+	numb = 0;
+
+	for (length = 0; b[length] != '\0'; length++)
+		;
+
+	for (length--, Base_Two = 1; length >= 0; length--, Base_Two *= 2)
+	{
+		if (b[length] != '0' && b[length] != '1')
+		{
+			return (0);
+		}
+
+		if (b[length] & 1)
+		{
+			numb += Base_Two;
+		}
+	}
+
+	return (numb);
+}
