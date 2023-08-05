4d69d4b22b77846c5b529fc59be40803a79c0ef8
owen20108 <owen20108@yahoo.com>
2023-08-04 19:52:57 -0700

    args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
  * main - add two numbers
@@ -7,30 +7,36 @@
   * @argv:array of string
   * Return: sum of all numbers
  */
-int main(int argc, char *argv[])
-{
-	int cntr, var, rslt;
 
-	rslt = 0;
-	if (argc > 1)
-	{
-		for (cntr = 1; cntr < argc; cntr++)
-		{
-			for (j = 0; argv[cntr][var] != '\0'; var++)
-			{
-				if (argv[cntr][var] >= '0' && argv[cntr][var] <= '9')
-				{
-					continue;
-				} else
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			rslt += atoi(argv[cntr]);
-		}
-		printf("%d\n", rslt);
-	} else
-		printf("0\n");
-	return (0);
+int main(int argc, char **argv)
+{
+int cntr, nmbr, suma = 0;
+char *arr;
+
+
+if (argc < 2)
+{
+printf("0\n");
+return (0);
 }
+
+
+for (cntr = 1; argv[cntr]; cntr++)
+{
+n = strtol(argv[cntr], &arr, 10);
+if (*arr)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+suma += nmbr;
+}
+}
+printf("%d\n", suma);
+
+
+return (0);
+}
+
