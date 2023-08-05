aae8c65bb09b0bfbf443f7abf3b85c02dd98a0c1
owen20108 <owen20108@yahoo.com>
2023-08-04 20:03:29 -0700

    args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,46 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main - add two numbers
-  * @argc:int
-  * @argv:array of string
-  * Return: sum of all numbers
- */
-
-int main(int argc, char **argv)
+  * main - Print the sum of args positive numbers
+  * @argc: int argument count
+  * @argv: list argument vector
+  *
+  * Return:  zero
+  */
+int main(int argc, char *argv[])
 {
-int cntr, nmbr, suma = 0;
-char *arr;
+	int cnt;
+	unsigned int q, suma = 0;
+	char *ary;
 
+	if (argc > 1)
+	{
+		for (cnt = 1; cnt < argc; cnt++)
+		{
+			ary = argv[cnt];
 
-if (argc < 2)
-{
-printf("0\n");
-return (0);
+			for (q = 0; q < strlen(e); q++)
+			{
+				if (ary[q] < 48 || ary[q] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			suma += atoi(ary);
+			ary++;
+		}
+
+		printf("%d\n", suma);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
 }
-
-
-for (cntr = 1; argv[cntr]; cntr++)
-{
-n = strtol(argv[cntr], &arr, 10);
-if (*arr)
-{
-printf("Error\n");
-return (1);
-}
-else
-{
-suma += nmbr;
-}
-}
-printf("%d\n", suma);
-
-
-return (0);
-}
-
