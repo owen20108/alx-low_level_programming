34ae38be30c1c4431681c39011ff2804354b7e27
owen20108 <owen20108@yahoo.com>
2023-08-08 07:52:08 -0700

    malloc free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - return a pointer for a new string 
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *duplcte;
+	unsigned int contr, length;
+
+	contr = 0;
+	length = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[length])
+		length++;
+
+	duplcte = malloc(sizeof(char) * (length + 1));
+
+	if (duplcte == NULL)
+		return (NULL);
+
+	while ((duplcte[contr] = str[contr]) != '\0')
+		contr++;
+
+	return (duplcte);
+}
