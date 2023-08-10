2dce77546bed6cb16e04a347c09a4e7237b5b1c2
owen20108 <owen20108@yahoo.com>
2023-08-10 09:00:49 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,34 +12,29 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *strng;
-	unsigned int cuntr1 = 0, cuntr2 = 0, lnght1 = 0, lnght2 = 0;
+	unsigned int cuntr1, cuntr2, lngth = 0, lngth2 = 0;
 
-	while (s1 && s1[lnght1])
-		lnght1++;
-	while (s2 && s2[lnght2])
-		lnght2++;
-
-	if (n < lnght2)
-		strng = malloc(sizeof(char) * (lnght1 + n + 1));
-	else
-		strng = malloc(sizeof(char) * (lnght1 + lnght2 + 1));
-
-	if (!strng)
-		return (NULL);
-
-	while (cuntr1 < lnght1)
+	if (s1 == NULL)
 	{
-		strng[cuntr1] = strng1[cuntr1];
-		cuntr1++;
+		s1 = "";
+		lngth++;
 	}
-
-	while (n < lnght2 && cuntr1 < (lnght1 + n))
-		strng[cuntr1++] = strng2[cuntr2++];
-
-	while (n >= lnght2 && cuntr1 < (lnght1 + lnght2))
-		strng[cuntr1++] = strng2[cuntr2++];
-
+	if (s2 == NULL)
+	{
+		s2 = "";
+		lngth2++;
+	}
+	for (cuntr1 = 0; s1[cuntr1] != '\0'; cuntr1++, lngth++)
+		;
+	for (cuntr1 = 0; s2[cuntr1] != '\0' &&  cuntr1 < n; cuntr1++, lngth2++)
+		;
+	strng = malloc((sizeof(char) * lngth) + (sizeof(char) * lngth2) + 1);
+	if (strng == NULL)
+		return (NULL);
+	for (cuntr1 = 0; s1[cuntr1] != '\0'; cuntr1++)
+		strng[cuntr1] = s1[cuntr1];
+	for (cuntr2 = 0; s2[cuntr2] != '\0' && cuntr2 < n; cuntr2++, cuntr1++)
+		strng[cuntr1] = s2[cuntr2];
 	strng[cuntr1] = '\0';
-
 	return (strng);
 }
