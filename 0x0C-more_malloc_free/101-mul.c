5330af70a816133e61c90003d94f3c706f31c6b9
owen20108 <owen20108@yahoo.com>
2023-08-11 07:50:01 -0700

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,10 +3,10 @@
 #include "main.h"
 
 /**
- * if_digit - test if a string has a non-digit char
- * @s: string will be evaluate
+ * if_digit - test a string contain  non-digit char
+ * @s: string will evaluate
  *
- * Return: 0 for a non-digit is found, 1 otherwise
+ * Return: 0 if a non-digit, 1 otherwise
  */
 int if_digit(char *s)
 {
@@ -22,10 +22,10 @@ int if_digit(char *s)
 }
 
 /**
- * _strlen - get  length of  string
- * @s: string will be evaluate
+ * _strlen - return  length of  string
+ * @s: string will evaluate
  *
- * Return: the length of the string
+ * Return:length string
  */
 int _strlnght(char *s)
 {
@@ -41,7 +41,7 @@ int _strlnght(char *s)
 /**
  * errors - fix error for main
  */
-void errors(void)
+void errorz(void)
 {
 	printf("Error\n");
 	exit(98);
@@ -49,51 +49,51 @@ void errors(void)
 
 /**
  * main - multiplie two positive numbers
- * @argc: number of  argument
- * @argv: array of argument
+ * @argc: number for argument
+ * @argv: array for argument
  *
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	char *strng1, *strng2;
-	int lngth1, lngth2, lngth, cuntr, getter, digits1, digits2, *rslt, z = 0;
+	int lngth1, lngth2, lngth, cuntr, getter, digitz1, digitz2, *rezlt, z = 0;
 
 	strng1 = argv[1], strng2 = argv[2];
 	if (argc != 3 || !if_digit(strng1) || !if_digit(strng2))
-		errors();
+		errorz();
 	lngth1 = _strlnght(strng1);
 	lngth2 = _strlnght(strng2);
 	lngth = lngth1 + lngth2 + 1;
-	rslt = malloc(sizeof(int) * lngth);
-	if (!rslt)
+	rezlt = malloc(sizeof(int) * lngth);
+	if (!rezlt)
 		return (1);
 	for (cuntr = 0; cuntr <= lngth1 + lngth2; cuntr++)
-		rslt[cuntr] = 0;
+		rezlt[cuntr] = 0;
 	for (lngth1 = lngth1 - 1; lngth1 >= 0; lngth1--)
 	{
-		digits1 = strng1[lngth1] - '0';
+		digitz1 = strng1[lngth1] - '0';
 		getter = 0;
 		for (lngth2 = _strlnght(strng2) - 1; lngth2 >= 0; lngth2--)
 		{
-			digits2 = strng2[lngth2] - '0';
-			getter += rslt[lngth1 + lngth2 + 1] + (digits1 * digits2);
-			rslt[lngth1 + lngth2 + 1] = getter % 10;
+			digitz2 = strng2[lngth2] - '0';
+			getter += rezlt[lngth1 + lngth2 + 1] + (digitz1 * digitz2);
+			rezlt[lngth1 + lngth2 + 1] = getter % 10;
 			getter /= 10;
 		}
 		if (getter > 0)
-			rslt[lngth1 + lngth2 + 1] += getter;
+			rezlt[lngth1 + lngth2 + 1] += getter;
 	}
 	for (cuntr = 0; cuntr < lngth - 1; cuntr++)
 	{
-		if (rslt[cuntr])
+		if (rezlt[cuntr])
 			z = 1;
 		if (z)
-			_putchar(rslt[cuntr] + '0');
+			_putchar(rezlt[cuntr] + '0');
 	}
 	if (!z)
 		_putchar('0');
 	_putchar('\n');
-	free(rslt);
+	free(rezlt);
 	return (0);
 }
