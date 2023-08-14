60ce8d761bbc3824fc1f6442a879c744a3b1f35e
owen20108 <owen20108@yahoo.com>
2023-08-14 08:16:12 -0700

    strct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog struct with variable
+ * @d: pointer for the struction
+ * @name: pointer for dog name
+ * @age: dog age
+ * @owner: pointer for owner age
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
