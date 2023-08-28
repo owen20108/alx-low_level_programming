754314db71cf4337ecdcc36a8e982b5da348b84c
owen20108 <owen20108@yahoo.com>
2023-08-28 13:44:44 -0700

    prntlst

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,32 +9,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int itrtr = 0;
-	listint_t *recent = *head;
-	listint_t *n_node;
+listint_t *n_node, *tmprry = *head;
+unsigned int itrtr;
 
-	while (itrtr < (idx - 1))
-	{
-		if (idx == 0)
-			break;
-		recent = recent->next;
-		itrtr++;
-	}
-	if (recent == NULL && idx != 0)
-		return (NULL);
-	n_node = malloc(sizeof(listint_t));
-	if (n_node == NULL)
-	{
-		return (NULL);
-	}
-	n_node->n = n;
-	if (idx == 0)
-	{
-		n_node->next = *head;
-		*head = recent;
-		return (n_node);
-	}
-	n_node->next = recent->next;
-	recent->next = n_node;
-	return (n_node);
+n_node = malloc(sizeof(listint_t));
+
+if (n_node == NULL)
+return (NULL);
+
+n_node->n = n;
+
+if (idx == 0)
+{
+n_node->next = tmprry;
+*head = n_node;
+return (n_node);
+}
+
+for (itrtr = 0; itrtr < (idx - 1); itrtr++)
+{
+if (tmprry == NULL || tmprry->next == NULL)
+return (NULL);
+
+tmprry = tmprry->next;
+}
+
+n_node->next = tmprry->next;
+tmprry->next = n_node;
+
+return (n_node);
 }
