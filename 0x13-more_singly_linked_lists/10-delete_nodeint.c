9e67e3822507ca412ceeaa3cc22b76c433e00f66
owen20108 <owen20108@yahoo.com>
2023-08-28 14:30:48 -0700

    prntlst

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,34 +1,40 @@
 #include "lists.h"
+
 /**
- * delete_nodeint_at_index - deletes a node at linked list
- * @head: pointer for head
- * @index: index location
- *
- * Return: 1 for succees otherwise -1 if it failed
+ * delete_nodeint_at_index - delete a node for index in linked list
+ * @head: start  of linked list
+ * @index: specified location
+ * Return: 1 if successful otherwise -1 if failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int itrtr = 0;
+	listint_t *prevos;
 	listint_t *recent = *head;
-	listint_t *next;
+	unsigned int anode;
 
-	while (itrtr < (index - 1))
-	{
-		if (index == 0)
-			break;
-		recent = recent->next;
-		itrtr++;
-	}
-	if (recent == NULL && index != 0)
+	if (!head || !(*head))
 		return (-1);
+
 	if (index == 0)
 	{
-		*head = recent->next;
+		*head = (*head)->next;
 		free(recent);
 		return (1);
 	}
-	next = recent->next->next;
-	free(recent->next);
-	recent->next = next;
-	return (1);
+
+	for (anode = 0; recent && (anode <= index); anode++)
+	{
+		if (anode == index)
+		{
+			prevos->next = recent->next;
+			free(recent);
+			return (1);
+		}
+		else
+		{
+			prevos = recent;
+			recent = recent->next;
+		}
+	}
+	return (-1);
 }
