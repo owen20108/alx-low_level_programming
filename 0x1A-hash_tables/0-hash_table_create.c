10b259dc09e9cc9391c10185bcadfbebb8f05ad4
owen20108 <owen20108@yahoo.com>
2023-11-03 16:07:14 -0700

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,25 +10,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table_created;
-    unsigned long int i;
+	hash_table_t *hash_table_created
+	unsigned long int i;
+	/* Allocate memory for the hash table */
+	hash_table_created = malloc(sizeof(hash_table_t));
+	if (hash_table_created == NULL)
+		return (NULL);
 
-    /* Allocate memory for the hash table */
-    hash_table_created = malloc(sizeof(hash_table_t));
+	hash_table_created->size = size;
+	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
 
-    if (hash_table_created == NULL)
-        return (NULL);
+	if (hash_table_created->array == NULL)
+		return (NULL);
 
-    hash_table_created->size = size;
-    hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+	for (i = 0; i < size; i++)
+	{
+		hash_table_created->array[i] = NULL;
+	}
 
-    if (hash_table_created->array == NULL)
-        return (NULL);
-
-    for (i = 0; i < size; i++)
-    {
-        hash_table_created->array[i] = NULL;
-    }
-
-    return (hash_table_created);
+	return (hash_table_created);
 }
